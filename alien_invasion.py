4dde1abe1146716a649cea7cc248767f4cb46bb8
Stefano <s.molari1@campus.unimib.it>
2025-08-21 18:28:35 +0200

    Add check for aliens hitting the bottom

--- alien_invasion.py
+++ alien_invasion.py
@@ -131,6 +131,9 @@ class AlienInvasion:
         if pygame.sprite.spritecollideany(self.ship, self.aliens):
             self._ship_hit()
 
+        # Look for aliens hitting the bottom of the screen
+        self._check_aliens_bottom()
+
     def _ship_hit(self):
         """Respond to the ship being hit by an alien."""
         # Decrement ships left.
@@ -146,6 +149,15 @@ class AlienInvasion:
 
         # Pause
         sleep(0.5)
+
+    def _check_aliens_bottom(self):
+        """Check if any aliens have reached the bottom of the screen."""
+        screen_rect = self.screen.get_rect()
+        for alien in self.aliens.sprites():
+            if alien.rect.bottom >= screen_rect.bottom:
+                # Treat this the same as if the ship got it
+                self._ship_hit()
+                break 
 
     def _create_fleet(self):
         """Create a fleet of aliens."""
