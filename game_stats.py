d29aed57dd5715d715c18e55ac49905bcd9dc393
Stefano <s.molari1@campus.unimib.it>
2025-08-22 11:53:34 +0200

    Add level

--- game_stats.py
+++ game_stats.py
@@ -16,3 +16,4 @@ class GameStats:
         """Initialize statistics tha can change during the game."""
         self.ships_left = self.settings.ship_limit
         self.score = 0
+        self.level = 1
--- scoreboard.py
+++ scoreboard.py
@@ -17,13 +17,14 @@ class Scoreboard:
         # Prepare the initial score image.
         self.prep_score()
         self.prep_high_score()
-        #self.prep_level()
+        self.prep_level()
         #self.prep_ships()
 
     def prep_score(self):
         """Turn the score into a rendered image."""
         rounded_score = round(self.stats.score, -1)
-        score_str = "{:,}".format(rounded_score)
+        score_str = "Score: "
+        score_str += "{:,}".format(rounded_score)
         self.score_image = self.font.render(score_str, True,
                 self.text_color, self.settings.bg_color)
 
@@ -35,7 +36,8 @@ class Scoreboard:
     def prep_high_score(self):
         """Turn the high score into a rendered image."""
         high_score = round(self.stats.high_score, -1)
-        high_score_str = f"{high_score:,}"
+        high_score_str = "Highest score: "
+        high_score_str += f"{high_score:,}"
         self.high_score_image = self.font.render(high_score_str, True,
                 self.text_color, self.settings.bg_color)
         
@@ -44,16 +46,17 @@ class Scoreboard:
         self.high_score_rect.centerx = self.screen_rect.centerx
         self.high_score_rect.top = self.score_rect.top
 
-    #def prep_level(self):
-    #    """Turn the level into a rendered image."""
-    #    level_str = str(self.stats.level)
-    #    self.level_image = self.font.render(level_str, True,
-    #            self.text_color, self.settings.bg_color)
-
-    #    # Position the level below the score.
-    #    self.level_rect = self.level_image.get_rect()
-    #    self.level_rect.right = self.score_rect.right
-    #    self.level_rect.top = self.score_rect.bottom + 10
+    def prep_level(self):
+        """Turn the level into a rendered image."""
+        level_str = "Level: "
+        level_str += str(self.stats.level)
+        self.level_image = self.font.render(level_str, True,
+                self.text_color, self.settings.bg_color)
+        
+        # Position the level below the score.
+        self.level_rect = self.level_image.get_rect()
+        self.level_rect.right = self.score_rect.right
+        self.level_rect.top = self.score_rect.bottom + 10
 
     #def prep_ships(self):
     #    """Show how many ships are left."""
@@ -74,5 +77,5 @@ class Scoreboard:
         """Draw scores, level, and ships to the screen."""
         self.screen.blit(self.score_image, self.score_rect)
         self.screen.blit(self.high_score_image, self.high_score_rect)
-        # self.screen.blit(self.level_image, self.level_rect)
+        self.screen.blit(self.level_image, self.level_rect)
         # self.ships.draw(self.screen)
